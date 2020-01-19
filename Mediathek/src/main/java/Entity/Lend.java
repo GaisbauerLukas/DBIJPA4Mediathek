a43abdf898e5ec808da05c2e9c09eab32d54a641
GaisbauerLukas <l.gaisbauer@students.htl-leonding.ac.at>
2020-01-19 21:10:27 +0100

    Feat: Added Constructor and GetterSetter Bill

--- Mediathek/src/main/java/Entity/Lend.java
+++ Mediathek/src/main/java/Entity/Lend.java
@@ -15,4 +15,43 @@ public class Lend {
 
     int costPerDay;
     Date dateOfAusleihe;
+
+    public Lend(int lendID, Customer customer, int costPerDay, Date dateOfAusleihe) {
+        this.lendID = lendID;
+        this.customer = customer;
+        this.costPerDay = costPerDay;
+        this.dateOfAusleihe = dateOfAusleihe;
+    }
+
+    public int getLendID() {
+        return lendID;
+    }
+
+    public void setLendID(int lendID) {
+        this.lendID = lendID;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public int getCostPerDay() {
+        return costPerDay;
+    }
+
+    public void setCostPerDay(int costPerDay) {
+        this.costPerDay = costPerDay;
+    }
+
+    public Date getDateOfAusleihe() {
+        return dateOfAusleihe;
+    }
+
+    public void setDateOfAusleihe(Date dateOfAusleihe) {
+        this.dateOfAusleihe = dateOfAusleihe;
+    }
 }
