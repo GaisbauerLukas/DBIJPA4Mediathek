69fd2809a1ff337e2cdeaa8bccf7c6b747148f38
GaisbauerLukas <gaisbauer.lukas@gmx.at>
2020-01-19 22:15:09 +0100

    DB update commit

---
+++ Mediathek/src/main/java/DTO/BillDTO.java
@@ -0,0 +1,26 @@
+package DTO;
+
+import Entity.Bill;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+
+public class BillDTO {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    String path = "../resources/csv/Bill.csv";
+    public void readFromCSV(){
+        new BufferedReader(new InputStreamReader(this.getClass()
+                .getResourceAsStream(path), Charset.defaultCharset()))
+                .lines()
+                .skip(1)
+                .map(s -> s.split(";"))
+                //.map(a -> new Bill())
+                .forEach(entityManager::merge);
+    }
+}
