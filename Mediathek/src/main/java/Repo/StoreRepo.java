a71792e49e4cb61f49253020b832250b140a3d58
FabianTischler <fabian.tischler@a1.net>
2020-01-19 21:40:50 +0100

    StoreRepo updated

--- Mediathek/src/main/java/Repo/StoreRepo.java
+++ Mediathek/src/main/java/Repo/StoreRepo.java
@@ -1,7 +1,28 @@
 package Repo;
 
+import Entity.Store;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Transactional
 public class StoreRepo {
+    @Inject
+    EntityManager em;
+
+    public void updateStore(Store store) {
+        em.createQuery("update Store set street = :street, employees = :employees, bills = :bills, location = :location " +
+                "where store.storeId = :id")
+                .setParameter("street", store.getStreet())
+                .setParameter("employees", store.getEmployees())
+                .setParameter("bills", store.getBills())
+                .setParameter("location", store.getLocation())
+                .setParameter("id", store.getStoreId());
+    }
+
+    public void deleteStore(long id) {
+        em.createQuery("delete from Store where Store.storeId = :storeid")
+                .setParameter("storeid", id);
+    }
 }
