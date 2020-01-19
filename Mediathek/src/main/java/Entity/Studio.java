ccea8b7e8734001004b3ad8c95e3c2349d95bf7a
PunzFlorian <punzflo@gmail.com>
2020-01-19 20:36:12 +0100

    added Studio entity

---
+++ Mediathek/src/main/java/Entity/Studio.java
@@ -0,0 +1,38 @@
+package Entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Studio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int studioId;
+    String studioName;
+    @ManyToOne
+    List<Movie> movies;
+
+    public int getStudioId() {
+        return studioId;
+    }
+
+    public void setStudioId(int studioId) {
+        this.studioId = studioId;
+    }
+
+    public String getStudioName() {
+        return studioName;
+    }
+
+    public void setStudioName(String studioName) {
+        this.studioName = studioName;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+}
