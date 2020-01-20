4c329662d8b618a5d964238c564dd39e7e675fab
PhilipTrinkl <philip.trinkl@gmail.com>
2020-01-20 11:13:33 +0100

    updated ReadIn in CustomerDTO

--- Mediathek/src/main/java/DTO/CustomerDTO.java
+++ Mediathek/src/main/java/DTO/CustomerDTO.java
@@ -1,14 +1,32 @@
 package DTO;
 
+import Entity.Actor;
+import Entity.Customer;
+import Entity.CustomerDetail;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 public class CustomerDTO {
     @PersistenceContext
-    EntityManager entityManager;
+    EntityManager em;
 
-    String path = "../resources/Bill.csv";
+    String path = "../resources/Customer.csv";
     public void readFromCSV(){
+        new BufferedReader(new InputStreamReader(this.getClass()
+                .getResourceAsStream(path), Charset.defaultCharset()))
+                .lines()
+                .skip(1)
+                .map(s -> s.split(";"))
+                .map(a -> new Customer(Integer.parseInt(a[0]), getbyId(Integer.parseInt(a[0])), Integer.parseInt(a[1])))
+                .forEach(em::merge);
+    }
 
+    public CustomerDetail getbyId(int id){
+        CustomerDetail cd = (CustomerDetail)em.createQuery("select cd from CustomerDetail cd where cd.CustomerId = :id").setParameter("id", id).getSingleResult();
+        return cd;
     }
 }
