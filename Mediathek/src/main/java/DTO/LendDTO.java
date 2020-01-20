4ede1214b2ada79b2887fd8634d076f48b64459e
PhilipTrinkl <philip.trinkl@gmail.com>
2020-01-20 11:48:38 +0100

    updated ReadIn in LendDTO

--- Mediathek/src/main/java/DTO/LendDTO.java
+++ Mediathek/src/main/java/DTO/LendDTO.java
@@ -1,14 +1,38 @@
 package DTO;
 
+import Entity.Customer;
+import Entity.Genre;
+import Entity.Lend;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Transient;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.sql.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class LendDTO {
     @PersistenceContext
-    EntityManager entityManager;
+    EntityManager em;
 
-    String path = "../resources/Bill.csv";
+
+
+    String path = "../resources/Lend.csv";
     public void readFromCSV(){
+        new BufferedReader(new InputStreamReader(this.getClass()
+                .getResourceAsStream(path), Charset.defaultCharset()))
+                .lines()
+                .skip(1)
+                .map(s -> s.split(";"))
+                .map(a -> new Lend(Integer.parseInt(a[0]),getbyId(Integer.parseInt(a[0])) ,Integer.parseInt(a[2]), Date.valueOf(a[3])))
+                .forEach(em::merge);
+    }
 
+    public Customer getbyId(int id){
+        Customer customer = (Customer)em.createQuery("select c from Customer c where c.getCustomerId() = :id").setParameter("id", id).getSingleResult();
+        return customer;
     }
 }
