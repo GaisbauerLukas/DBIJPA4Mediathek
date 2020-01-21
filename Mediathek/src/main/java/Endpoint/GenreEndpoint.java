0075efc93a01cc95ff5c2f86b34babe3331fe430
GaisbauerLukas <l.gaisbauer@students.htl-leonding.ac.at>
2020-01-21 22:18:59 +0100

    Feat: Added GenreEndpoint

---
+++ Mediathek/src/main/java/Endpoint/GenreEndpoint.java
@@ -0,0 +1,33 @@
+package Endpoint;
+
+import Entity.Actor;
+import Entity.Genre;
+import Repos.GenreRepo;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+
+@Path("api")
+public class GenreEndpoint {
+
+    @Inject
+    GenreRepo genreRepo;
+
+    @Path("create")
+    @POST
+    public void create(Genre genre) {
+        genreRepo.createGenre(genre);
+    }
+
+    @Path("update")
+    @PUT
+    public void update(Genre genre){
+        genreRepo.updateGenre(genre);
+    }
+
+    @Path("delete-{id}")
+    @DELETE
+    public void delete(@PathParam("id") int id){
+        genreRepo.deleteGenre(id);
+    }
+}
