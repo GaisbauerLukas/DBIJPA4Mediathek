13632fff08c7fa8fc2a87cc2831d135d1f026f25
FabianTischler <fabian.tischler@a1.net>
2020-01-21 12:37:55 +0100

    added changes in LendRepo

--- Mediathek/src/main/java/Repos/LendRepo.java
+++ Mediathek/src/main/java/Repos/LendRepo.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 @Transactional
@@ -30,15 +31,17 @@ public class LendRepo {
         em.persist(lend);
     }
     public int getCostOfLend(int id){
-        Date date = (Date)em.createQuery("select dateOfAusleihe from Lend  where  lendID = :Id").setParameter("Id",id).getSingleResult();
-        int daysoff=0;
+        GregorianCalendar date = (GregorianCalendar)em.createQuery("select dateOfAusleihe from Lend  where  lendID = :Id").setParameter("Id",id).getSingleResult();
+        GregorianCalendar today = new GregorianCalendar();
         int cost=(int)em.createQuery("select costPerDay from Lend where lendID = :Id").setParameter("Id",id).getSingleResult();
+        int daysoff=(int)(today.getTimeInMillis()-date.getTimeInMillis()) / (1000*60*60*24);
         return cost*daysoff;
     }
     public int getCostOfLend(Lend lend){
-        Date date = (Date)em.createQuery("select dateOfAusleihe from Lend  where  lendID = :Id").setParameter("Id",lend.getLendID()).getSingleResult();
-        int daysoff=0;
+        GregorianCalendar date = (GregorianCalendar)em.createQuery("select dateOfAusleihe from Lend  where  lendID = :Id").setParameter("Id",lend.getLendID()).getSingleResult();
+        GregorianCalendar today = new GregorianCalendar();
         int cost=(int)em.createQuery("select costPerDay from Lend where lendID = :Id").setParameter("Id",lend.getLendID()).getSingleResult();
+        int daysoff=(int)(today.getTimeInMillis()-date.getTimeInMillis()) / (1000*60*60*24);
         return cost*daysoff;
     }
     public void createLendsByList(List<Lend> lends){
