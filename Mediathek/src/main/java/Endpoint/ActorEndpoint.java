0607f12627ddad28c18e21d54a9e0290bd9e2206
GaisbauerLukas <l.gaisbauer@students.htl-leonding.ac.at>
2020-01-21 22:14:43 +0100

    Feat: Added BillEndpoint

--- Mediathek/src/main/java/Endpoint/ActorEndpoint.java
+++ Mediathek/src/main/java/Endpoint/ActorEndpoint.java
@@ -4,10 +4,7 @@ import Entity.Actor;
 import Repos.ActorRepo;
 
 import javax.inject.Inject;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
@@ -31,7 +28,7 @@ public class ActorEndpoint {
         }
 
         @Path("delete-{id}")
-        @PUT
+        @DELETE
         public void delete(@PathParam("id") int id){
             actorRepo.deleteActor(id);
         }
---
+++ Mediathek/src/main/java/Endpoint/CustomerEndpoint.java
@@ -0,0 +1,35 @@
+package Endpoint;
+
+import Entity.Actor;
+import Entity.Bill;
+import Entity.Customer;
+import Entity.Lend;
+import Repos.CustomerRepo;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+
+@Path("api")
+public class CustomerEndpoint {
+    @Inject
+    CustomerRepo customerRepo;
+
+    @Path("create")
+    @POST
+    public void create(Customer customer){
+        customerRepo.createCustomer(customer);
+    }
+
+
+    @Path("update")
+    @PUT
+    public void update(Customer customer){
+        customerRepo.updateCustomer(customer);
+    }
+
+    @Path("delete-{id}")
+    @DELETE
+    public void delete(@PathParam("id") int id){
+        customerRepo.deleteCustomer(id);
+    }
+}
