0f2d1ec56b5cb85e2f08ec0eee278ea2464f773b
PhilipTrinkl <philip.trinkl@gmail.com>
2020-01-21 17:06:16 +0100

    configure Path of File in StoreDTO

--- Mediathek/src/main/java/DTO/StoreDTO.java
+++ Mediathek/src/main/java/DTO/StoreDTO.java
@@ -24,7 +24,7 @@ public class StoreDTO {
     public StoreDTO() {
     }
 
-    String path = "../resources/Bill.csv";
+    String path = "../resources/Store.csv";
     public void readFromCSV(){
         new BufferedReader(new InputStreamReader(this.getClass()
                 .getResourceAsStream(path), Charset.defaultCharset()))
