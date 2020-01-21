888fa4e30b3b82287423d022ffee3f8aadb9de7d
PhilipTrinkl <philip.trinkl@gmail.com>
2020-01-21 10:17:49 +0100

    configured Movie, Store and Studio DTO's

--- Mediathek/src/main/java/DTO/StoreDTO.java
+++ Mediathek/src/main/java/DTO/StoreDTO.java
@@ -1,13 +1,28 @@
 package DTO;
 
+import Entity.Bill;
+import Entity.Employee;
+import Entity.Location;
 import Entity.Store;
 
 import javax.persistence.EntityManager;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 public class StoreDTO {
+    int storeId;
+    String street;
+    List<Employee> employees;
+    List<Bill> bills;
+    Location location;
+
     @PersistenceContext
     EntityManager entityManager;
+
+    public StoreDTO() {
+    }
 
     String path = "../resources/Bill.csv";
     public void readFromCSV(){
@@ -17,5 +32,45 @@ public class StoreDTO {
     public Store getById(int id){
         //return (Store)entityManager.createQuery("SELECT store from Store store where storeId = :Id").setParameter("Id", id).getSingleResult();
         return null;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Bill> getBills() {
+        return bills;
+    }
+
+    public void setBills(List<Bill> bills) {
+        this.bills = bills;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
     }
 }
