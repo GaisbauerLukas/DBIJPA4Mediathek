8365ba12c4b7315e4f2ab71a6dd7d70188730592
PhilipTrinkl <philip.trinkl@gmail.com>
2020-01-21 16:35:05 +0100

    configure StoreDTO

--- Mediathek/src/main/java/DTO/StoreDTO.java
+++ Mediathek/src/main/java/DTO/StoreDTO.java
@@ -1,14 +1,14 @@
 package DTO;
 
-import Entity.Bill;
-import Entity.Employee;
-import Entity.Location;
-import Entity.Store;
+import Entity.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.PersistenceContext;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.List;
 
 public class StoreDTO {
@@ -19,19 +19,60 @@ public class StoreDTO {
     Location location;
 
     @PersistenceContext
-    EntityManager entityManager;
+    EntityManager em;
 
     public StoreDTO() {
     }
 
     String path = "../resources/Bill.csv";
     public void readFromCSV(){
-
+        new BufferedReader(new InputStreamReader(this.getClass()
+                .getResourceAsStream(path), Charset.defaultCharset()))
+                .lines()
+                .skip(1)
+                .map(s -> s.split(";"))
+                .map(a -> new Store(a[1], getEmployeesbyId(a[2].split(",")),getBillsbyId(a[3].split(",")), getLocationbyId(Integer.parseInt(a[4])), getLendsbyId(a[5].split(",")), getMoviesbyId(a[6].split(","))))
+                .forEach(em::merge);
     }
 
-    public Store getById(int id){
-        //return (Store)entityManager.createQuery("SELECT store from Store store where storeId = :Id").setParameter("Id", id).getSingleResult();
-        return null;
+    public List<Employee> getEmployeesbyId(String[] ids){
+        List<Employee> employees = null;
+        for(int i = 0; i < ids.length; i++){
+            employees.add((Employee)em.createQuery("select e from Employee e where e.empId = :id").setParameter("id", Integer.parseInt(ids[i])).getSingleResult());
+        }
+        return employees;
+    }
+
+    public List<Bill> getBillsbyId(String[] ids){
+        List<Bill> bills = null;
+        for(int i = 0; i < ids.length; i++){
+            bills.add((Bill)em.createQuery("select b from Bill b where b.billId = :id").setParameter("id", Integer.parseInt(ids[i])).getSingleResult());
+        }
+
+        return bills;
+    }
+
+    public Location getLocationbyId(int id){
+        Location location = (Location)em.createQuery("select l from Location l where l.locationId = :id").setParameter("id", id).getSingleResult();
+        return location;
+    }
+
+    public List<Lend> getLendsbyId(String[] ids){
+        List<Lend> lends = null;
+        for(int i = 0; i < ids.length; i++){
+            lends.add((Lend)em.createQuery("select l from Lend l where l.lendID = :id").setParameter("id", Integer.parseInt(ids[i])).getSingleResult());
+        }
+
+        return lends;
+    }
+
+    public List<Movie> getMoviesbyId(String[] ids){
+        List<Movie> movies = null;
+        for(int i = 0; i < ids.length; i++){
+            movies.add((Movie)em.createQuery("select m from Movie m where m.movieId = :id").setParameter("id", Integer.parseInt(ids[i])));
+        }
+
+        return movies;
     }
 
     public int getStoreId() {
